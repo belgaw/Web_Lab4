30b55a14dbd75e7fef68349a008d34687e09241d
Belisov Gleb <112969122+belgaw@users.noreply.github.com>
2025-12-30 06:11:58 +0300

    Delete js directory

--- js/storage.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = 'weather_cities';
-
-export function saveCities(cities) {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(cities));
-}
-
-export function loadCities() {
-  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-}
